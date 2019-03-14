50b82a362aa0499199c62b1dbb63c6ca1074a88f
chenyu <535509852@qq.com>
2019-03-15 01:11:12 +0800

    日常提交

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## angular7-mysql-crud (前后端)
 - client (ng7)
 - database (初始化sql)
-- server (express + ts)
+- server (express + mysql +ts)
 
 ## 截图
 ![](screenshot/screenshot.png)
--- server/src/database.ts
+++ server/src/database.ts
@@ -2,6 +2,7 @@ import mysql from "promise-mysql";
 
 import keys from "./keys";
 
+// 创建连接池
 const pool = mysql.createPool(keys.database);
 
 pool.getConnection().then(connection => {
@@ -9,4 +10,52 @@ pool.getConnection().then(connection => {
   console.log("DB is Connected");
 });
 
+// 一般的mysql
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+// host : '',
+// user : '',
+// password : '',
+// port: '',
+// database: '',
+// });
+// connection.connect()
+// var userAddSql = 'INSERT INTO userinfo(Id,UserName,UserPass) VALUES(0,?,?)';
+// var userAddSql_Params = ['Wilson', 'abcd'];
+// // 增加
+// connection.query(userAddSql,userAddSql_Params,function(error,res){
+// connection.end() // 还有destory方法
+// })
+// // 更新
+// var userModSql = 'UPDATE userinfo SET UserName = ?,UserPass = ? WHERE Id = ?';
+// var userModSql_Params = ['张三', '5678',1];
+// connection.query(userAddSql,userAddSql_Params,function(error,res){
+
+// })
+// // 查询
+// var userGetSql = 'SELECT * FROM userinfo';
+// connection.query(userGetSql,function(error,res){
+
+// })
+// // 删除
+// var userDelSql = 'DELETE FROM userinfo';
+// connection.query(userDelSql,function(error,res){
+
+// })
+
+// //监听connection事件
+// pool.on('connection', function(connection) {
+// connection.query();
+// });
+
+// //直接使用
+// pool.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+// if (err) throw err;
+// console.log('The solution is: ', rows[0].solution);
+// });
+
+// // 共享
+// pool.getConnection(function(err, connection) {
+// // connected!
+// });
 export default pool;
