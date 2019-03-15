33e5d2ec4b4b61feb37a9180c699b4940d0efb39
chenyu <535509852@qq.com>
2019-03-15 23:12:00 +0800

    日常提交

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -3,26 +3,29 @@ import { Request, Response } from "express";
 import pool from "../database";
 
 class GamesController {
+  // 查询列表
   public async list(req: Request, res: Response): Promise<void> {
     const games = await pool.query("SELECT * FROM games");
     res.json(games);
   }
 
+  // 查询单条
   public async getOne(req: Request, res: Response): Promise<any> {
     const { id } = req.params;
     const games = await pool.query("SELECT * FROM games WHERE id = ?", [id]);
-    console.log(games.length);
     if (games.length > 0) {
       return res.json(games[0]);
     }
     res.status(404).json({ text: "The game doesn't exits" });
   }
 
+  // 创建
   public async create(req: Request, res: Response): Promise<void> {
     const result = await pool.query("INSERT INTO games set ?", [req.body]);
     res.json({ message: "Game Saved" });
   }
 
+  // 更新
   public async update(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     const oldGame = req.body;
@@ -30,6 +33,7 @@ class GamesController {
     res.json({ message: "The game was Updated" });
   }
 
+  // 删除
   public async delete(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     await pool.query("DELETE FROM games WHERE id = ?", [id]);
